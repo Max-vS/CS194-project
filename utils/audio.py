8e68bea1d38f0a9dc5fd10dcd43e0bdbe4c55b19
Max-vS <max@vonstorch.com>
2024-12-15 20:20:25 -0800

    remove unnecessary utils from audio

--- utils/audio.py
+++ utils/audio.py
@@ -1,14 +1,10 @@
 from gtts import gTTS
-from playsound import playsound
 import whisper
 import tempfile
 import os
-import sounddevice as sd
-import wavio
 import pyttsx3
-from pathlib import Path
 from openai import OpenAI
-    
+
 # Initialize Whisper model (you may choose different models like "small", "medium", etc.)
 whisper_model = whisper.load_model("base")
 
@@ -23,14 +19,6 @@ whisper_model = whisper.load_model("base")
 #     os.remove(audio_path)
 
 
-def play_audio(text, speed=200):
-    """Convert text to speech with adjustable speed and play it."""
-    engine = pyttsx3.init()
-    engine.setProperty('rate', speed)  # Default is usually ~150-200 words per minute
-    engine.say(text)
-    engine.runAndWait()
-    
-    
 def st_play_audio(text):
     """Save tts audio and return to streamlit frontend"""
     tts = gTTS(text=text, lang="en")
@@ -55,38 +43,15 @@ def st_play_audio_openai(text):
     return filepath
 
 
-def record_audio(duration=5, filename="user_response.wav"):
-    """Record audio from user and save it to a file."""
-    fs = 44100  # Sample rate
-    print("Recording audio... (speak your response)")
-    audio_data = sd.rec(int(duration * fs), samplerate=fs, channels=1, dtype='int16')
-    sd.wait()  # Wait until recording is finished
-    wavio.write(filename, audio_data, fs, sampwidth=2)
-    print(f"Audio saved at: {filename}")
-    return filename
-
-
 def transcribe_audio(filename="user_response.wav"):
     """Transcribe audio response from user using Whisper."""
     if not os.path.exists(filename):
-        print(f"Error: {filename} not found. Please ensure the recording was saved correctly.")
+        print(f"Error: {
+              filename} not found. Please ensure the recording was saved correctly.")
         return ""
     try:
-        transcription = whisper_model.transcribe(filename, language = 'en')
+        transcription = whisper_model.transcribe(filename, language='en')
         return transcription['text']
     except FileNotFoundError:
         print("Audio file not found. Please ensure recording is working.")
         return ""
-
-
-def st_transcribe_audio(filename):
-    if not os.path.exists(filename):
-        print(f"Error: {filename} not found. Please ensure the recording was saved correctly.")
-        return ""
-    try:
-        whisper_stt = whisper_model.transcribe(filename, language='en')
-        return whisper_stt
-    except FileNotFoundError:
-        print("Audio file not found. Please ensure recording is working.")
-        return ""
-    
